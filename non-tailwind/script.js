d43805ab2c3fc39a8e74f234a30c548a84fb4b41
Jayrowwwww <jerrehsalera77@gmail.com>
2025-03-01 20:46:19 +0800

    Tentative Finals

--- non-tailwind/script.js
+++ non-tailwind/script.js
@@ -1,11 +1,11 @@
 const openBtn = document.getElementById("openModal");
-const closeCtn = document.getElementById("closeModal");
-const Modal = document.getElementById("modal");
+const closeBtn = document.getElementById("closeModal");
+const modal = document.getElementById("modal");
 
 openBtn.addEventListener("click", () => {
-    Modal.classList.add("open");
+    modal.classList.add("open");
 })
 
 closeBtn.addEventListener("click", () => {
-    Modal.classList.remove("open");
+    modal.classList.remove("open");
 })
